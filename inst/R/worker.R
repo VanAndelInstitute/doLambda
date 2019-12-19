304d3a249074379d3f54eb2253765a14b97e277d
Eric Kort <eric.kort@vai.org>
2019-12-19 12:48:34 -0500

    worker debug

--- inst/R/worker.R
+++ inst/R/worker.R
@@ -9,9 +9,6 @@ lambdaWorker <- function(bucket, key) {
   s3saveRDS(res, key, bucket)  
 }
 
-#args = commandArgs(trailingOnly=TRUE)
-#print(args)
-
 id <- basename(tempfile(pattern=""))
 ROOT <- Sys.getenv("AWS_LAMBDA_RUNTIME_API")
 EVENT_DATA <- httr::GET(paste0("http://", ROOT, "/2018-06-01/runtime/invocation/next"))
@@ -22,17 +19,13 @@ bucket <- res$Records[[1]]$s3$bucket$name
 key <- res$Records[[1]]$s3$object$key
 aws.s3::s3saveRDS(EVENT_DATA, paste0("eventdata_", id, "_", 1, ".rds"), "jlab-test-4")
 
+print(paste0("Running job with key ", key, " in bucket ", bucket, "."))
+lambdaWorker(key, bucket)  
+print("Done.")
+
 httr::POST(paste0("http://", 
-           ROOT, 
-           "/2018-06-01/runtime/invocation/",
-           REQUEST_ID, 
-           "/response"),
-           body = "OK")
-
-#library(doLambda)
-#library(aws.s3)
-#lambdaWorker(args[1], args[2])  
-
-# how do we know if we are done?
-EVENT_DATA <- httr::GET(paste0("http://", ROOT, "/2018-06-01/runtime/invocation/next"))
-aws.s3::s3saveRDS(EVENT_DATA, paste0("eventdata_", id, "_", 2, ".rds"), "jlab-test-4")
+                  ROOT, 
+                  "/2018-06-01/runtime/invocation/",
+                  REQUEST_ID, 
+                  "/response"),
+           body = "OK!")
