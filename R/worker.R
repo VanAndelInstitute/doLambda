ed8d7aabb59da5a41e27db6f9311c1bf290f07b7
Eric Kort <eric.kort@vai.org>
2019-12-19 09:53:06 -0500

    lambdaWorker direct call

--- R/worker.R
+++ R/worker.R
@@ -2,23 +2,26 @@
 #'
 #' Retrieve job and execute it.
 #'
+#' @param bucket name of an S3 bucket you own that will hold the job queue
+#' @param key AWS Access Key. 
+#' 
 #' @note This function is automatically called (by the boostrap script) when the 
-#' lambda job is triggered. It typically is never used on the client end. 
+#' lambda job is triggered. It typically is never used on the client end. It is
+#' assumed that the lambda function is executed by a role that has read and write
+#' permissions for the specified S3 bucket.
 #'
 #' @return nothing. Called for side effect of doing a unit of work.
 #' @importFrom aws.s3 s3readRDS s3saveRDS
 #' @export
-lambdaWorker <- function() {
-  root <- Sys.getenv("AWS_LAMBDA_RUNTIME_API")
-  url <- paste0("http://", root, "/2018-06-01/runtime/invocation/next")
-  res <- GET(url)
-  payload <- content(res)
-  
-  key <- payload$Records[[1]]$s3$object$key
-  bucket <- payload$Records[[1]]$s3$bucket$name
+lambdaWorker <- function(bucket, key) {
   dat <- s3readRDS(key, bucket)  
-
   res <- eval(dat$expr, dat$envir, dat$enclos)
   key <- gsub("jobs/", "outs/", key)
   s3saveRDS(res, key, bucket)  
+}
+
+# call lambdaWorker if this script was sourced directly 
+# (as is done by our lambda bootstrap script)
+if(exists("args") && length(args) == 2) {
+  lambdaWorker(args[1], args[2])  
 }
